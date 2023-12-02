6f79292cbf72df2906ffb9d6f61eac4913fa562a
benjaminrall <benr2508@aol.com>
2023-12-02 10:58:10 +0000

    Day 2

---
+++ day-2/1.py
@@ -0,0 +1,39 @@
+
+
+TESTING = 0
+
+def parse_set(s):
+    cubes = [0, 0, 0]
+    for info in s.split(','):
+        digit = ''
+        for c in info:
+            if c.isnumeric():
+                digit += c
+            else:
+                cubes['rgb'.index(c)] = int(digit)
+                break
+    return cubes
+
+def parse_input(line):
+    cubes = [0, 0, 0] # R, G, B
+
+    line = line[line.index(':') + 1:]
+
+    sets = [parse_set(s) for s in line.split(';')]
+    for s in sets:
+        cubes[0] = max(cubes[0], s[0])
+        cubes[1] = max(cubes[1], s[1])
+        cubes[2] = max(cubes[2], s[2])
+    return cubes
+
+
+with open("test.txt" if TESTING else "input.txt", "r") as f:
+    lines = [parse_input(line.strip().replace(" ", "")) for line in f.readlines()]
+
+total = 0
+for i, line in enumerate(lines):
+    if line[0] > 12 or line[1] > 13 or line[2] > 14:
+        continue
+    total += i + 1
+
+print(total)
