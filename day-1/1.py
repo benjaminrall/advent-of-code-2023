6cfa2f30e3774770c0e73ac1b2d8fdf793db5ce0
benjaminrall <benr2508@aol.com>
2023-12-02 03:24:57 +0000

    Day 1

--- day-1/1.py
+++ day-1/1.py
@@ -1,16 +1,4 @@
-numbers_strings =  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
-
 with open("input.txt", "r") as f:
-    lines = [[c for c in line[:-1] if c in '123456789' or c in numbers_strings] for line in f.readlines()]
+    lines = [[c for c in line[:-1] if c.isnumeric()] for line in f.readlines()]
 
-total = 0
-for value in lines:
-    v0 = value[0]
-    if value[0] not in '123456789':
-        v0 = str(numbers_strings.index(value[0]) + 1)
-    v1 = value[-1]
-    if value[-1] not in '123456789':
-        v1 = str(numbers_strings.index(value[-1]) + 1)
-    total += int(v0 + v1)
-
-print(total)
+print(sum([int(value[0] + value[-1]) for value in lines]))
--- day-1/2.py
+++ day-1/2.py
@@ -1,31 +1,32 @@
-numbers_strings =  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '1', '2', '3', '4', '5', '6', '7', '8', '9']
+NUMBERS =  ['one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', '1', '2', '3', '4', '5', '6', '7', '8', '9']
 
 with open("input.txt", "r") as f:
     lines = [line.strip() for line in f.readlines()]
 
 total = 0
 for line in lines:
-    v0 = None
-    v1 = None
-    for i, s in enumerate(numbers_strings):
+    first, last = None, None
+
+    # Tests for each possible number (both spelled out words and single characters)
+    for i, s in enumerate(NUMBERS):
         try:
-            # Get first and last occurence
+            # Gets first and last occurence of the number
             indices = [line.index(s), line.rindex(s)]
             
-            # Convert written numbers to number characters
-            char = s
-            L = len(s)
-            if L > 1:
-                char = str(i + 1)
+            # Convert written out numbers to single number characters
+            char = str(i + 1) if len(s) > 1 else s
 
-            # Assign min and max numbers
-            if v0 is None or indices[0] < v0[0]:
-                v0 = (indices[0], char)
-            if v1 is None or indices[-1] + L > v1[0]:
-                v1 = (indices[-1] + L, char)
+            # Reassign first and last numbers to the current number 
+            # if it's at a smaller/larger position than them
+            if first is None or indices[0] < first[0]:
+                first = (indices[0], char)
+            if last is None or indices[-1] > last[0]:
+                last = (indices[-1], char)
+
+        # Empty except to just ignore cases where a number doesn't exist
         except:
             continue
-    v = int(v0[1] + v1[1])
-    total += v
+
+    total += int(first[1] + last[1])
 
 print(total)
